dbb0658d5efc964b6b90128a59f716db0df7fd97
jil <jil.p@simformsolutions.com>
2023-09-20 17:31:34 +0530

    add: interface serialization

---
+++ InterfaceSerialization/Models/Entities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSerialization.Models
+{
+    public interface ICar
+    {
+        public string? Brand
+        {
+            get;
+            set;
+        }
+    }
+    public interface IBmw : ICar
+    {
+        public string? Model
+        {
+            get;
+            set;
+        }
+    }
+    public class CarDetails : IBmw
+    {
+        public string? Brand
+        {
+            get;
+            set;
+        }
+        public string? Model
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ InterfaceSerialization/Program.cs
@@ -0,0 +1,24 @@
+﻿using InterfaceSerialization.Models;
+using System.Text.Json;
+
+namespace InterfaceSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Feature 1: Interface serialization
+            IBmw detail = new CarDetails
+            {
+                Brand = "BMV Benz",
+                Model = "AMG-6HG"
+            };
+
+            var serializedValue = JsonSerializer.Serialize(detail);
+            Console.WriteLine(serializedValue);
+
+            Console.ReadKey();
+
+        }
+    }
+}
