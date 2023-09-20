9ac2cd1a75f9f60cf1891eb1243e4c026ea9f680
jil <jil.p@simformsolutions.com>
2023-09-20 17:29:16 +0530

    add: frozen dictionary type

---
+++ FrozenDictionaryType/FrozenDictionaryType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrozenDictionaryType
+{
+    internal class FrozenDictionaryType
+    {
+        private static readonly FrozenDictionary<string, string> staticData =
+            LoadStaticData().ToFrozenDictionary();
+
+        private static Dictionary<string, string> LoadStaticData()
+        {
+            return new Dictionary<string, string>() { { "Key1", "Value1" }, { "Key2", "Value2" }, { "Key3", "Value3" } };
+
+        }
+
+        internal string GetValue(string key)
+        {
+            
+            if (staticData.TryGetValue(key, out string? keyValue))
+            {
+                return keyValue;
+            }
+
+            return string.Empty;
+        }
+
+    }
+}
---
+++ FrozenDictionaryType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FrozenDictionaryType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                new dictionary type which improves the performance of read operations.
+                you are not allowed to make any changes to the keys and values once the collection is created.
+             */
+
+            FrozenDictionaryType frozenDictionaryType = new FrozenDictionaryType();
+
+            Console.WriteLine(frozenDictionaryType.GetValue("key1"));
+
+            Console.ReadKey();
+        }
+    }
+}
