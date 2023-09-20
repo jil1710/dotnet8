7e1ff2661e7da0f469d038d0ebf433574681e47c
jil <jil.p@simformsolutions.com>
2023-09-20 17:52:20 +0530

    add: builtin guard clause in .net 8

---
+++ BuiltInGuardClauses/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BuiltInGuardClauses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? name = null;
+            int age = 0;
+
+            // old way
+            if(name == null )
+            {
+                throw new ArgumentNullException("name is null");
+            }
+
+
+            // new  way
+            ArgumentNullException.ThrowIfNull("name is null");
+            //or
+            ArgumentNullException.ThrowIfNullOrEmpty("name is null or empty");
+
+
+            // old way
+            if(age < 18)
+            {
+                throw new ArgumentOutOfRangeException(" Age is nor less than 18");
+            }
+
+            // new way
+            ArgumentOutOfRangeException.ThrowIfLessThan(age, 18);
+        }
+    }
+}
