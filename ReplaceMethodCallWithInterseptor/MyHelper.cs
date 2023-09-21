30941093f826c67bae25e24ad4eb82dfa3ab0e3e
jil <jil.p@simformsolutions.com>
2023-09-21 14:43:38 +0530

    add: interceptor to call another method

---
+++ ReplaceMethodCallWithInterseptor/MyHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+        [AttributeUsage(AttributeTargets.Method,AllowMultiple =true)]
+        internal sealed class InterceptsLocationAttribute : Attribute
+        {
+            public InterceptsLocationAttribute(string filePath,int line,int col)
+            {
+        
+            }
+        }
+    
+}
---
+++ ReplaceMethodCallWithInterseptor/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ReplaceMethodCallWithInterseptor
+{
+
+    public static class GenerateInterseptor
+    {
+        [System.Runtime.CompilerServices.InterceptsLocation("""C:\Users\jil\source\repos\dotnet8\ReplaceMethodCallWithInterseptor\Program.cs""", 32,17)]
+        public static void MyInterseptorMethod(this Example example,string msg)
+        {
+            Console.WriteLine("Message from interseptor : " + msg);
+        }
+    }
+
+    public class Example
+    {
+        public void Greet(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Now i want to intersept the 2 call to with another call then i use .net 8 interseptor feature
+
+            var exp = new Example();
+            exp.Greet("Good morning");
+            exp.Greet("Good night"); // Message form interseptor Good night
+            exp.Greet("Good evening");
+        }
+    }
+}
