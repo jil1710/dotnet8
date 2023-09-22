c2c705437784195e88c40eb0bc067926242e9eab
jil <jil.p@simformsolutions.com>
2023-09-22 18:24:16 +0530

    add: short circuiting the middleware execution

---
+++ ShoetCircuitInDotNet8/Program.cs
@@ -0,0 +1,62 @@
+
+namespace ShoetCircuitInDotNet8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            app.Use(async (req, next) =>
+            {
+                Console.WriteLine("Invoke every time in http pipeline");
+               await next();
+            });
+
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+
+            // i want to stop this below route execute direct without invoking the middleware
+            // In simple work for this below route i am doing short circuit that help to improve the performance by skipping the middleware which is not important for specific route
+            app.MapGet("/health", () =>
+            {
+                return Results.Ok("Good health");
+            }).ShortCircuit();
+
+            app.Run();
+        }
+    }
+}
